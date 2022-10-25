345520efb597de1db29a32f08d4ed13ae8bbeab1
EmanuelPatricio <emanuelptr81@gmail.com>
2022-10-25 09:38:43 -0400

    V1.0

---
+++ Data/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesAPI.Data
+{
+  public class EmployeeDbContext : DbContext
+  {
+    public EmployeeDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+  }
+}
